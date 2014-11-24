5439bb11ed67464f32b3293699804df9f74b5a1a
spanhal1 <shp.panhalesanket18@gmail.com>
2014-11-24 00:20:33 -0500

    Bug Fix: proc4: added exceptions as required

--- proc4.sql
+++ proc4.sql
@@ -9,7 +9,24 @@ e.classid = cls.classid and
 cls.dept_code = c.dept_code and
 cls.course# = c.course#;
 s_rec stu_cursor%rowtype;
+
+invalid_sid exception;
+no_course exception;
+studentcount number;
+coursecount number;
+
 begin
+	studentcount := 0;
+	coursecount := 0;
+
+	select count(*) into studentcount from students where sid = p_sid;
+	select count(*) into coursecount from enrollments where sid = p_sid;
+	
+	if(studentcount = 0) then
+		raise invalid_sid;
+	elsif(coursecount = 0) then
+		raise no_course;
+	end if;
         if(not stu_cursor%isopen) then
                 open stu_cursor;
         end if;
@@ -19,6 +36,12 @@ begin
                 fetch stu_cursor into s_rec;
         end loop;
         close stu_cursor;
+
+	exception
+		when invalid_sid then
+			 dbms_output.put_line('The sid is invalid.');
+		when no_course then
+			dbms_output.put_line('The student has not taken any course.');
 end;
 /
 
