ab302e3fb3689dc4a37fb0a140994b46d936187a
root <root@debianChaitanya.chaitanyaBU.org>
2014-11-23 20:22:13 -0500

    Added procedure 2

--- proc2.sql
+++ proc2.sql
@@ -1,54 +1,79 @@
 set serveroutput on;
-create or replace procedure insertstudent
-(p_sid in students.sid%type,
-p_firstname in students.firstname%type,
-p_lastname in students.lastname%type,
-p_status in students.status%type,
-p_gpa in students.gpa%type,
-p_email in students.email%type)is
-
-	invalid_gpa exception;
-	invalid_sid exception;
-	invalid_firstname exception;
-	invalid_lastname exception;
-	invalid_status exception;
-	invalid_email exception;
-	emailcount number(2);
+create or replace procedure show_students is 
+cursor stu_cursor is select * from students;
+s_rec stu_cursor%rowtype;
 begin
-emailcount := 0;
-select count(*) into emailcount from students where email = p_email;
-if(p_gpa < 0 or p_gpa > 4) then
-	raise invalid_gpa;
-elsif(p_sid not like 'B%') then
-	raise invalid_sid;
-elsif(p_firstname is null) then
-	raise invalid_firstname;
-elsif(p_lastname is null) then
-	raise invalid_lastname;
-elsif(p_status not in ('freshman','sophomore','junior','senior','graduate')) then
-	raise invalid_status;
-elsif(emailcount > 0) then
-	raise invalid_email;
-else
-	insert into students values (p_sid,p_firstname,p_lastname,p_status,p_gpa,p_email);
-end if;
-
-exception
-	when invalid_gpa then
-		dbms_output.put_line('GPA value is must be in between 0 and 4.');
-	when invalid_sid then
-		dbms_output.put_line('Sid value is must start with B.');
-	when invalid_firstname then
-		dbms_output.put_line('firstname value cannot be null.');
-	when invalid_lastname then
-		dbms_output.put_line('lastname value can not be null.');
-	when invalid_status then
-		dbms_output.put_line('status value must be freshman,sophomore,junior,senior,graduate');
-	when invalid_email then
-		dbms_output.put_line('email value is not unique.');
+	if(not stu_cursor%isopen) then
+		open stu_cursor;
+	end if;
+	fetch stu_cursor into s_rec;
+	while(stu_cursor%found) loop
+		dbms_output.put_line(s_rec.sid||','||s_rec.firstname||','||s_rec.lastname||','||s_rec.status||','||s_rec.gpa||','||s_rec.email);
+		fetch stu_cursor into s_rec;
+	end loop;
+	close stu_cursor;
 end;
 /
-show error
 
-
-
+create or replace procedure show_courses is 
+cursor cou_cursor is select * from courses;
+c_rec cou_cursor%rowtype;
+begin
+	if(not cou_cursor%isopen) then
+		open cou_cursor;
+	end if;
+	fetch cou_cursor into c_rec;
+	while(cou_cursor%found) loop
+		dbms_output.put_line(c_rec.dept_code||','||c_rec.course#||','||c_rec.title);
+		fetch cou_cursor into c_rec;
+	end loop;
+	close cou_cursor;
+end;
+/
+create or replace procedure show_prereq is 
+cursor pre_cursor is select * from prerequisites;
+p_rec pre_cursor%rowtype;
+begin
+	if(not pre_cursor%isopen) then
+		open pre_cursor;
+	end if;
+	fetch pre_cursor into p_rec;
+	while(pre_cursor%found) loop
+		dbms_output.put_line(p_rec.dept_code||','||p_rec.course#||','||p_rec.pre_dept_code||','||p_rec.pre_course#);
+		fetch pre_cursor into p_rec;
+	end loop;
+	close pre_cursor;
+end;
+/
+create or replace procedure show_classes is 
+cursor cla_cursor is select * from classes;
+c_rec cla_cursor%rowtype;
+begin
+	if(not cla_cursor%isopen) then
+		open cla_cursor;
+	end if;
+	fetch cla_cursor into c_rec;
+	while(cla_cursor%found) loop
+		dbms_output.put_line(c_rec.classid||','||c_rec.dept_code||','||c_rec.course#||','||c_rec.sect#||','||c_rec.year||','||c_rec.semester||','||c_rec.limit||','||c_rec.class_size);
+		fetch cla_cursor into c_rec;
+	end loop;
+	close cla_cursor;
+end;
+/
+create or replace procedure show_enrollments is 
+cursor enrol_cursor is select * from enrollments;
+e_rec enrol_cursor%rowtype;
+begin
+	if(not enrol_cursor%isopen) then
+		open enrol_cursor;
+	end if;
+	fetch enrol_cursor into e_rec;
+	while(enrol_cursor%found) loop
+		dbms_output.put_line(e_rec.sid||','||e_rec.classid||','||e_rec.lgrade);
+		fetch enrol_cursor into e_rec;
+	end loop;
+	close enrol_cursor;
+end;
+/
+show errors;
+>>>>>>> Stashed changes
